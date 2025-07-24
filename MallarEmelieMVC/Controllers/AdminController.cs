fe13fa00d0e94f2dda2536ebd1a3531bd9c9e2ab
skolden1 <skolden123@hotmail.com>
2025-07-24 12:18:13 +0200

    began w adminctrl,  and a createform  w validations

--- MallarEmelieMVC/Controllers/AdminController.cs
+++ MallarEmelieMVC/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MallarEmelieMVC.Data;
+using MallarEmelieMVC.Data.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MallarEmelieMVC.Controllers
 {
@@ -7,9 +10,56 @@ namespace MallarEmelieMVC.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly AppDbContext _context;
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet] //för varje post metod behöver vi en getmetod, då postmetod tar emot ett formulär, och det är genom denna getmetod vi visar formen.
         public async Task<IActionResult> CreateTemplate()
         {
+
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTemplate(Template template, IFormFile ImageUpload)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(template);
+            }
+
+            if (ImageUpload != null && ImageUpload.Length > 0)
+            {
+                //för o hitta till mappen templatepics som ligger i wwwroot
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "templatePics");
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                // filnamn
+                var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(ImageUpload.FileName);
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageUpload.CopyToAsync(stream);
+                }
+
+                
+                template.ImageUrl = "/templatePics/" + uniqueFileName;
+            }
+
+            _context.Templates.Add(template);
+            await _context.SaveChangesAsync();
+
+            TempData["Success"] = "Mallen har lagts till!";
+            return RedirectToAction("CreateTemplate");
         }
     }
 }
