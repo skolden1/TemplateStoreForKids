fedd60c6d6755439571fb78ece6a13f54a2152e2
skolden1 <skolden123@hotmail.com>
2025-08-14 16:01:09 +0200

    Alot of css, did some responsive work, still not happy w some pieces, started w the cartController and added / updated some more db tables, next step is to fix all crud operations for that and maybe fix an icon for cart aswell w all the items etc.

---
+++ MallarEmelieMVC/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using MallarEmelieMVC.Areas.Identity.Models;
+using MallarEmelieMVC.Data;
+using MallarEmelieMVC.Data.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MallarEmelieMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly UserManager<IdentityUserTable> _userManager;
+        private readonly AppDbContext _context;
+        public CartController(UserManager<IdentityUserTable> userManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+
+        //[HttpGet]
+        //public async Task<IActionResult> AddToCart()
+        //{
+        //    var user 
+        //}
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCart(int templateId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if(user == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var userId = user.Id;
+
+            var existingCartItem = await _context.CartItems.FirstOrDefaultAsync(c => c.UserId == userId && c.TemplateId == templateId);
+
+            if(existingCartItem != null)
+            {
+                existingCartItem.Quantity += 1;
+            }
+            else
+            {
+                var newCartItem = new CartItem
+                {
+                    UserId = userId,
+                    TemplateId = templateId,
+                    Quantity = 1
+                };
+
+                _context.CartItems.Add(newCartItem);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Cart");
+        }
+    }
+}
